bcbf17908088bd6df6d8ee2f65820c834815e5d5
thoughtspile <v.klepov@gmail.com>
2015-08-23 15:29:04 +0300

    Added pseudo-Perlin

--- terragen.js
+++ terragen.js
@@ -1,4 +1,4 @@
-(function() {
+var terragen = (function() {
 	var h = 257;
 	var w = 257;
 	var height = new Float32Array(h * w);
@@ -20,19 +20,7 @@
 			}
 		};
 	}());
-		
-	var smooth = function(src, to, w, h) {
-		for (var i = 0; i < w; i++) {
-			for (var j = 0; j < h; j++) {
-				var left = src[(i == 0? w - 1: i - 1) * h + j];
-				var right = src[(i == w - 1? 0: i + 1) % w * h + j];
-				var up = src[i * h + (j == h - 1? 0: j + 1)];
-				var down = src[i * h + (j == 0? h - 1: j - 1)];
-				to[i * h + j] = (left + right + up + down)/ 4;
-			}
-		}
-	};
-		
+				
 	var range = function(arr) {
 		var low = Infinity;
 		var high= -Infinity;
@@ -82,7 +70,41 @@
 	}
 
 	
+	var octaveNoise = function(raw) {
+		// opts: high, low, decay, ease
+		var buff = new Float32Array(raw.length);
+		for (var i = 0; i < buff.length; i++) {
+			buff[i] = Math.random();
+		}
+		for (var i = 0; i < raw.length; i++) {
+			raw[i] = 0;
+		}
+		for (var step = 256; step >= 1; step /= 2) {
+			for (var x = 0; x < w; x++) {
+				for (var y = 0; y < h; y++) {
+					var srcX = x / step;
+					var srcY = y / step;
+					var fracX = srcX - Math.floor(srcX);
+					//fracX = fracX * fracX * (3 - 2 * fracX);
+					var fracY = srcY - Math.floor(srcY);
+					//fracY = fracY * fracY * (3 - 2 * fracY);
+					var x1 = (Math.floor(srcX) + w) % w;
+					var y1 = (Math.floor(srcY) + h) % h;
+					var x2 = (x1 + w - 1) % w;
+					var y2 = (y1 + h - 1) % h;
+					
+					raw[x + y * w] += 
+						(fracX * fracY * buff[x1 + y1 * w] +
+						fracX * (1 - fracY) * buff[x1 + y2 * w] +
+						(1 - fracX) * fracY * buff[x2 + y1 * w] +
+						(1 - fracX) * (1 - fracY) * buff[x2 + y2 * w]) * step / 64;
+				}
+			}
+		}
+	};
+	
 	var raise = function(raw) {
+		// opts: offset dist
 		var posx = Math.floor(w * Math.random());
 		var posy = Math.floor(h * Math.random());
 		var ang = Math.tan(Math.PI * (Math.random() - .5));
@@ -97,6 +119,7 @@
 	};
 		
 	var hill = function(raw, rmin, rmax) {
+		// opts: rmin, rmax: rdist
 		var posx = Math.floor(w * Math.random());
 		var posy = Math.floor(h * Math.random());
 		var r = rmin + Math.random() * (rmax - rmin);
@@ -114,6 +137,7 @@
 	}
 	
 	var diamondsqr = function(arr) {
+		// opts: seed count, decay
 		arr[0] = Math.random();
 		arr[h - 1] = Math.random();
 		arr[h * (w - 1)] = Math.random();
@@ -147,6 +171,10 @@
 	};
 
 	
+	var bind = function(mapEl) {
+		
+	};
+	
 	var gen = function(mode, count, context) {
 		if (mode === 'fl')
 			for (var i = 0; i < count; i++)
@@ -156,13 +184,19 @@
 				hill(height, 3, 80);
 		else if (mode === 'ds')
 			diamondsqr(height);
+		else if (mode === 'on')
+			octaveNoise(height);
 		//cutoff(height, 0);
 		//map(height, function(x) { return x * x * x; });
 		display(height, context);
 	};
 	
-	window.gen = gen;
+	return {
+		bind: bind,
+		run: gen
+	};
 }());
+
 
 (function() {
 	var modeEl = document.getElementById('mode');
@@ -176,9 +210,9 @@
 	mapEl.height = h;
 	
 	runBtn.addEventListener('click', function() {
-		gen(modeEl.value, parseInt(iterEl.value), mapContext);
+		terragen.run(modeEl.value, parseInt(iterEl.value), mapContext);
 	});
 	modeEl.addEventListener('change', function() {
-		iterEl.disabled = (modeEl.value === 'ds');
+		iterEl.disabled = (['ds', 'on'].indexOf(modeEl.value) !== -1);
 	});
 }());
