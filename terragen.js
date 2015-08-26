f0928fd1ba7815bae691acde869aca4776cc8fcd
thoughtspile <v.klepov@gmail.com>
2015-08-26 10:52:51 +0300

    Perlin uses permutation table

--- terragen.js
+++ terragen.js
@@ -1,6 +1,6 @@
 var terragen = (function() {
-	var h = 257;
-	var w = 257;
+	var h = 513;
+	var w = 513;
 	var height = new Float32Array(h * w);
 	
 	
@@ -69,6 +69,12 @@ var terragen = (function() {
 		return raw;
 	}
 
+	var reset = function(raw) {
+		for (var i = 0; i < raw.length; i++) {
+			raw[i] = 0;
+		}		
+	};
+	
 	
 	var octaveNoise = function(raw) {
 		// opts: high, low, decay, ease
@@ -76,18 +82,15 @@ var terragen = (function() {
 		for (var i = 0; i < buff.length; i++) {
 			buff[i] = Math.random();
 		}
-		for (var i = 0; i < raw.length; i++) {
-			raw[i] = 0;
-		}
 		for (var step = 256; step >= 1; step /= 2) {
 			for (var x = 0; x < w; x++) {
 				for (var y = 0; y < h; y++) {
 					var srcX = x / step;
 					var srcY = y / step;
 					var fracX = srcX - Math.floor(srcX);
-					//fracX = fracX * fracX * (3 - 2 * fracX);
+					fracX = fracX * fracX * (3 - 2 * fracX);
 					var fracY = srcY - Math.floor(srcY);
-					//fracY = fracY * fracY * (3 - 2 * fracY);
+					fracY = fracY * fracY * (3 - 2 * fracY);
 					var x1 = (Math.floor(srcX) + w) % w;
 					var y1 = (Math.floor(srcY) + h) % h;
 					var x2 = (x1 + w - 1) % w;
@@ -98,6 +101,136 @@ var terragen = (function() {
 						fracX * (1 - fracY) * buff[x1 + y2 * w] +
 						(1 - fracX) * fracY * buff[x2 + y1 * w] +
 						(1 - fracX) * (1 - fracY) * buff[x2 + y2 * w]) * step / 64;
+				}
+			}
+		}
+	};
+	
+	var funNonperlin = function(raw) {
+		// opts: high, low, decay, ease
+		var gx = new Float32Array(raw.length);
+		var gy = new Float32Array(raw.length);
+		for (var i = 0; i < raw.length; i++) {
+			gx[i] = Math.random();
+			gy[i] = Math.random();
+		}
+		for (var step = 8; step >= 8; step /= 2) {
+			for (var x = 0; x < w; x++) {
+				for (var y = 0; y < h; y++) {
+					var srcX = x / step;
+					var srcY = y / step;
+					var fracX = srcX - Math.floor(srcX);
+					var fracY = srcY - Math.floor(srcY);
+					var x1 = Math.floor(srcX);
+					var y1 = Math.floor(srcY);
+					var x2 = (x1 + 1) % w;
+					var y2 = (y1 + 1) % h;
+					
+					var s = fracX * gx[x1 + y1 * w] +
+						fracY * gy[x1 + y1 * w];
+					var u = fracX * gx[x1 + y2 * w] +
+						(1 - fracY) * gy[x1 + y2 * w];
+					var t = (1 - fracX) * gx[x2 + y1 * w] +
+						fracY * gy[x2 + y1 * w];
+					var v = (1 - fracX) * gx[x2 + y2 * w] +
+						(1 - fracY) * gy[x2 + y2 * w];
+						
+					var easeX = fracX * fracX * (3 - 2 * fracX);
+					var easeY = fracY * fracY * (3 - 2 * fracY);
+					var a = s + easeX * (t - s);
+					var b = u + easeX * (v - u);
+					raw[x + y * w] += a + easeY * (b - a);
+				}
+			}
+		}
+	};
+	
+	var perlin = function(raw) {
+		// opts: high, low, decay, ease
+		var grads = 100;
+		var gx = new Float32Array(grads);
+		var gy = new Float32Array(grads);
+		for (var i = 0; i < grads; i++) {
+			var dir = 2 * Math.PI * (Math.random());
+			gx[i] = Math.cos(dir);
+			gy[i] = Math.sin(dir);
+		}
+		var perms = Math.max(w, h);
+		var perm = new Float32Array(2 * perms);
+		for (var i = 0; i < perm.length; i++) {
+			perm[i] = Math.floor(perms * Math.random());
+		}
+		var permute = function(x, y) {
+			return perm[x + perm[y]] % grads;
+		}
+		
+		for (var step = 256; step >= 1; step /= 2) {
+			for (var x = 0; x < w; x++) {
+				for (var y = 0; y < h; y++) {
+					var srcX = x / step;
+					var srcY = y / step;
+					var x1 = Math.floor(srcX);
+					var y1 = Math.floor(srcY);
+					var dx = srcX - x1;
+					var dy = srcY - y1;
+					var x2 = (x1 + 1) % w;
+					var y2 = (y1 + 1) % h;
+										
+					var s = dx * gx[permute(x1, y1)] +
+						dy * gy[permute(x1, y1)];
+					var u = dx * gx[permute(x1, y2)] +
+						(dy - 1) * gy[permute(x1, y2)];
+					var t = (dx - 1) * gx[permute(x2, y1)] +
+						dy * gy[permute(x2, y1)];
+					var v = (dx - 1) * gx[permute(x2, y2)] +
+						(dy - 1) * gy[permute(x2, y2)];
+						
+					var easeX = dx * dx * (3 - 2 * dx);
+					var easeY = dy * dy * (3 - 2 * dy);
+					var a = s + easeX * (t - s);
+					var b = u + easeX * (v - u);
+					raw[x + y * w] += (a + easeY * (b - a)) * step / 64;
+				}
+			}
+		}
+	};
+		
+	var improvedPerlin = function(raw) {
+		// opts: high, low, decay, ease
+		var gx = new Float32Array(raw.length);
+		var gy = new Float32Array(raw.length);
+		for (var i = 0; i < raw.length; i++) {
+			var dir = 2 * Math.PI * (Math.random());
+			gx[i] = Math.cos(dir);
+			gy[i] = Math.sin(dir);	
+		}
+		for (var step = 256; step >= 1; step /= 2) {
+			for (var x = 0; x < w; x++) {
+				for (var y = 0; y < h; y++) {
+					var srcX = x / step;
+					var srcY = y / step;
+					var x1 = Math.floor(srcX);
+					var y1 = Math.floor(srcY);
+					var dx = srcX - x1;
+					var dy = srcY - y1;
+					var x2 = (x1 + 1) % w;
+					var y2 = (y1 + 1) % h;
+					
+					
+					var s = dx * gx[x1 + y1 * w] +
+						dy * gy[x1 + y1 * w];
+					var u = dx * gx[x1 + y2 * w] +
+						(dy - 1) * gy[x1 + y2 * w];
+					var t = (dx - 1) * gx[x2 + y1 * w] +
+						dy * gy[x2 + y1 * w];
+					var v = (dx - 1) * gx[x2 + y2 * w] +
+						(dy - 1) * gy[x2 + y2 * w];
+						
+					var easeX = dx * dx * (3 - 2 * dx);
+					var easeY = dy * dy * (3 - 2 * dy);
+					var a = s + easeX * (t - s);
+					var b = u + easeX * (v - u);
+					raw[x + y * w] += (a + easeY * (b - a)) * step / 64;
 				}
 			}
 		}
@@ -176,6 +309,7 @@ var terragen = (function() {
 	};
 	
 	var gen = function(mode, count, context) {
+		reset(height);
 		if (mode === 'fl')
 			for (var i = 0; i < count; i++)
 				raise(height, 3);
@@ -186,10 +320,15 @@ var terragen = (function() {
 			diamondsqr(height);
 		else if (mode === 'on')
 			octaveNoise(height);
+		else if (mode === 'pn')
+			perlin(height);
+		else if (mode === 'ipn')
+			improvedPerlin(height);
 		//cutoff(height, 0);
 		//map(height, function(x) { return x * x * x; });
 		display(height, context);
 	};
+	
 	
 	return {
 		bind: bind,
@@ -204,8 +343,8 @@ var terragen = (function() {
 	var runBtn = document.getElementById('run');
 	var mapEl = document.getElementById('map');
 	var mapContext = mapEl.getContext('2d');
-	var w = 257;
-	var h = 257;
+	var w = 513;
+	var h = 513;
 	mapEl.width = w;
 	mapEl.height = h;
 	
@@ -213,6 +352,6 @@ var terragen = (function() {
 		terragen.run(modeEl.value, parseInt(iterEl.value), mapContext);
 	});
 	modeEl.addEventListener('change', function() {
-		iterEl.disabled = (['ds', 'on'].indexOf(modeEl.value) !== -1);
+		iterEl.disabled = (['ds', 'on', 'pn'].indexOf(modeEl.value) !== -1);
 	});
 }());
